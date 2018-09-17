b3ae67b49596dc6b5d0c4738086e7449d456fe12
Christian <crusone3@gmail.com>
2018-09-17 17:14:03 -0400

    Primer commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content=" ">
+	<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!-- <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> -->
+
+    <title>EKA Studios</title>
+
+ 	<!-- Materialize CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Custom css  -->
+    <link href="/css/style.css" rel="stylesheet">
+</head>
+<body>
+	<header>
+		<nav class="nav-extended align-center">
+			<img src="/images/logo.svg" id="nav-logo" alt="Logo">
+			<div id="nav-info">
+				<h2>BIENVENIDOS A EKA STUDIO</h2>
+				<h3>CUERNAVACA | CIUDAD DE MÉXICO | CARACAS</h3>
+				<p>ENVÍANOS TUS PREGUNTAS A HELLO@EKASTUDIO.NET</p>
+				<p id="nav-date">13//26</p>
+			</div>
+			<div id="nav-links">
+				<ul>
+				    <li><a href="#">ACERCA DE EKA</a></li>
+				    <li><a href="#">SERVICIOS</a></li>
+				    <li><a href="#">PROYECTOS</a></li>
+				    <li><a href="#">CONTACTO</a></li>
+				</ul>
+			</div>
+		</nav>
+
+		<ul class="sidenav" id="mobile-demo">
+			<li><a href="#">ACERCA DE EKA</a></li>
+		    <li><a href="#">SERVICIOS</a></li>
+		    <li><a href="#">PROYECTOS</a></li>
+		    <li><a href="#">CONTACTO</a></li>
+		</ul>
+	</header>
+
+	<div id="main">
+
+		<section id="jumbotron">
+			<img src="/images/index/header.png" alt="header">
+		</section>
+
+		<section id="comofunciona">
+			<div class="row">
+				<div class="col m6" id="comofunciona-title">
+					<h4>¿CÓMO<br>FUNCIONA?</h4>
+				</div>
+				<div class="col m6" id="comofunciona-info">
+					<div class="comofunciona-paso">
+						<div class="comofunciona-paso-n"><p>PASO<br>01_</p></div>
+						<div class="comofunciona-paso-info">
+							<h5>INGRESA TUS DATOS</h5>
+							<p>ASÍ SABREMOS SI SOLO NECESITAS UN LOGOTIPO O UNA ESTRATEGIA ESPECÍFICA.</p>
+						</div>
+						<div class="comofunciona-paso-icono">
+							<img src="/images/index/como_funciona_icono_1.svg" alt="icono">
+						</div>
+					</div>
+				</div>
+				<div class="col m6" id="comofunciona-info">
+					<div class="comofunciona-paso">
+						<div class="comofunciona-paso-n"><p>PASO<br>02_</p></div>
+						<div class="comofunciona-paso-info">
+							<h5>OBTÉN TU PRESUPUESTO EN MINUTOS</h5>
+							<p>ARMA UN PLAN DE ACUERDO A TUS REQUERIMIENTOS Y OBTEN TU PRESUPUESTO AL INSTANTE. <b>¡ES INMEDIATO!</b></p>
+						</div>
+						<div class="comofunciona-paso-icono">
+							<img src="/images/index/como_funciona_icono_2.svg" alt="icono">
+						</div>
+					</div>
+				</div>
+				<div class="col m6" id="comofunciona-info">
+					<div class="comofunciona-paso">
+						<div class="comofunciona-paso-n"><p>PASO<br>03_</p></div>
+						<div class="comofunciona-paso-info">
+							<h5>ENVIA TU SOLICITUD</h5>
+							<p>¡TE ATENDEREMOS EL MISMO DÍA! NUESTRO HORARIO ES DE 9.00-18.00</p>
+						</div>
+						<div class="comofunciona-paso-icono">
+							<img src="/images/index/como_funciona_icono_3.svg" alt="icono">
+						</div>
+					</div>
+				</div>
+				<a href="#" class="btn-custom">¡ARMA UN PLAN A TU MEDIDA AHORA MISMO!</a>
+			</div>
+		</section>
+
+		<div class="section-divider"></div>
+
+		<section id="descuentos">
+			<h4>¿Y...TIENEN DESCUENTOS?</h4>
+			<div class="row">
+				<div class="col m6" id="descuentos-title">
+					<h5>¡¡ MIENTRAS MÁS GRANDE SEA TU PLAN...<br>...MAYOR SERÁ TU DESCUENTO !!</h5>
+				</div>
+				<div class="vertical-divider"></div>
+				<div class="col m6" id="descuentos-descuentos">
+					<h5>3 ITEMS <span>-&nbsp;&nbsp;</span> 5% DE DESCUENTO</h5>
+					<h5>5 ITEMS <span>-</span> 10% DE DESCUENTO</h5>
+					<h5>7 ITEMS <span>-</span> 15% DE DESCUENTO</h5>
+				</div>
+			</div>
+			<div id="descuentos_arma-tu-plan" class="z-depth-4">
+				<h5>ARMA <br>TU PLAN</h5>
+				<div id="arma-tu-plan-planes">
+					<div class="arma-tu-plan_plan">
+						<div class="arma-tu-plan_plan-img"><img src="/images/index/arma-tu-plan-1.svg" alt="arma-tu-plan-icon"></div>
+						<div class="arma-tu-plan_plan-checkbox">
+							<p>
+						      <label>
+						        <input type="checkbox" class="filled-in" />
+						        <span></span>
+						      </label>
+						    </p>
+						</div>
+					</div>
+					<div class="arma-tu-plan_plan">
+						<div class="arma-tu-plan_plan-img"><img src="/images/index/arma-tu-plan-2.png" alt="arma-tu-plan-icon"></div>
+						<div class="arma-tu-plan_plan-checkbox">
+							<p>
+						      <label>
+						        <input type="checkbox" class="filled-in" />
+						        <span></span>
+						      </label>
+						    </p>
+						</div>
+					</div>
+					<div class="arma-tu-plan_plan">
+						<div class="arma-tu-plan_plan-img"><img src="/images/index/arma-tu-plan-3.png" alt="arma-tu-plan-icon"></div>
+						<div class="arma-tu-plan_plan-checkbox">
+							<p>
+						      <label>
+						        <input type="checkbox" class="filled-in" />
+						        <span></span>
+						      </label>
+						    </p>
+						</div>
+					</div>
+					<div class="arma-tu-plan_plan">
+						<div class="arma-tu-plan_plan-img"><img src="/images/index/arma-tu-plan-4.png" alt="arma-tu-plan-icon"></div>
+						<div class="arma-tu-plan_plan-checkbox">
+							<p>
+						      <label>
+						        <input type="checkbox" class="filled-in" />
+						        <span></span>
+						      </label>
+						    </p>
+						</div>
+					</div>
+					<div class="arma-tu-plan_plan">
+						<div class="arma-tu-plan_plan-img"><img src="/images/index/arma-tu-plan-5.png" alt="arma-tu-plan-icon"></div>
+						<div class="arma-tu-plan_plan-checkbox">
+							<p>
+						      <label>
+						        <input type="checkbox" class="filled-in" />
+						        <span></span>
+						      </label>
+						    </p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="descuentos_subtotal" class="z-depth-4">
+				<div class="row">
+					<div class="col m6 left-align">
+						<h5>SUBTOTAL POR 3 ITEMS</h5>
+					</div>
+					<div class="col m6 right-align">
+						<p>$ 15,000</p>
+					</div>
+				</div>
+			</div>
+			<div id="descuentos_descuento" class="z-depth-4">
+				<div class="row">
+					<div class="col m4 left-align">
+						<h5>-5% DE DESCUENTO</h5>
+					</div>
+					<div class="col m4">
+						<h6>SUBTOTAL POR 3 ITEMS</h6>
+					</div>
+					<div class="col m4 right-align">
+						<p>-$ 1,500</p>
+					</div>
+				</div>
+			</div>
+			<div id="descuentos_total" class="z-depth-4">
+				<div id="descuentos_descuento_lanzamiento">
+					<div class="row">
+						<div class="col m6 left-align">
+							<p>DESCUENTO POR LANZAMIENTO <span>&nbsp;(TIEMPO LIMITADO)</span></p>
+						</div>
+						<div class="col m6 right-align">
+							<p>-$ 850</p>
+						</div>
+					</div>
+				</div>
+				<div id="total">
+					<div class="row">
+						<div class="col m6 left-align">
+							<h5>TOTAL</h5>
+						</div>
+						<div class="col m6 right-align">
+							<p><span>$ 12,650</span></p>
+							<p><span class="ahorro">AHORRO: $ 2,350</span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="otras_opciones">
+			<h4>OTRAS OPCIONES CON DESCUENTO</h4>
+			<h5>SIGUE LOS PASOS PARA TENER TU PRESUPUESTO</h5>
+			<div class="row">
+				<div class="col m6">
+					<div class="opcion z-depth-4">
+						<h5>SOLO LOGO</h5>
+						<img src="/images/index/arma-tu-plan-1.svg" alt="opcion">
+						<a href="#" class="btn-custom">INICIAR</a>
+						<h4>-5% DE DESCUENTO</h4>
+					</div>
+				</div>
+				<div class="col m6">
+					<div class="opcion opcion-dos z-depth-4">
+						<h5>LOGO + 3 APLICACIONES</h5>
+						<img src="/images/index/arma-tu-plan-1.svg" alt="opcion">
+						<img src="/images/index/arma-tu-plan-3.png" alt="opcion">
+						<a href="#" class="btn-custom">INICIAR</a>
+						<h4>-10% DE DESCUENTO</h4>
+					</div>
+				</div>
+			</div>
+			<div class="opcion opcion-tres z-depth-4">
+				<h5>LOGO + APLICACIONES + WEBSITE</h5>
+				<img src="/images/index/arma-tu-plan-1.svg" alt="opcion">
+				<img src="/images/index/arma-tu-plan-2.png" alt="opcion">
+				<img src="/images/index/arma-tu-plan-5.png" alt="opcion">
+				<a href="#" class="btn-custom">INICIAR</a>
+				<h4>-15% DE DESCUENTO</h4>
+			</div>
+			<div id="especifico">
+				<h5>¿BUSCAS ALGO MÁS ESPECÍFICO?</h5>
+				<h4>¡ARMA TU PLAN A MEDIDA!</h4>
+				<a href="#" class="btn-custom">CONTINUAR</a>
+			</div>
+		</section>
+
+	</div>
+
+	<footer>
+		<div id="social-media">
+			<img src="/images/corona.svg" alt="corona">
+			<div class="row">
+				<div class="col m4"><a href="#">FACEBOOK</a></div>
+				<div class="col m4"><a href="#">INSTAGRAM</a></div>
+				<div class="col m4"><a href="#">LINKEDIN</a></div>
+			</div>
+			<h6>SÍGUENOS EN NUESTRAS REDES SOCIALES</h6>
+		</div>
+		<div id="info">
+			<h5>CUERNAVACA | CIUDAD DE MÉXICO | CARACAS</h5>
+			<p>ENVÍANOS TUS PREGUNTAS A HELLO@EKASTUDIO.NET</p>
+			<h4>SERVICIOS</h4>
+			<h3>Somos un equipo de profesionales que trabaja para crear y posicionar tu marca.</h3>
+		</div>
+		<div id="links">
+			<div class="row">
+				<div class="col m3">
+					<ul class="link-list">
+					    <li class="link-title"><a href="#">Copywriting</a></li>
+					    <li><a href="#">Nombre</a></li>
+					    <li><a href="#">Eslogan</a></li>
+					    <li><a href="#">Redacción</a></li>
+					</ul>
+				</div>
+				<div class="col m3">
+					<ul class="link-list">
+					    <li class="link-title"><a href="#">Diseño Gráfico</a></li>
+					    <li><a href="#">Logotipo</a></li>
+					    <li><a href="#">Aplicaciones de Marca</a></li>
+					    <li><a href="#">Señalización</a></li>
+					    <li><a href="#">Manual de Marca</a></li>
+					</ul>
+				</div>
+				<div class="col m3">
+					<ul class="link-list">
+					    <li class="link-title"><a href="#">Ilustración</a></li>
+					    <li><a href="#">Diseño de íconos</a></li>
+					    <li><a href="#">Diseño de infografías</a></li>
+					</ul>
+				</div>
+				<div class="col m3">
+					<ul class="link-list">
+					    <li class="link-title"><a href="#">Diseño Web</a></li>
+					    <li><a href="#">Diseño de interface</a></li>
+					    <li><a href="#">Experiencia de usuario</a></li>
+					    <li><a href="#">Programación</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<!-- Materialize JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
